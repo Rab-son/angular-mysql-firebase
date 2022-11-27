82695443d436ca79b7b99da360509907f35b300e
Rab-son <rabsonsayendajnr@gmail.com>
2022-11-27 02:09:19 +0200

    listing detail page

--- buy-and-sell/src/app/app.module.ts
+++ buy-and-sell/src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
-import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
 import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
 import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
 import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
+import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListingsPageComponent,
-    ListingsDetailPageComponent,
     MyListingsPageComponent,
     NewListingsPageComponent,
     EditListingPageComponent,
-    ContactPageComponent
+    ContactPageComponent,
+    ListingDetailPageComponent
   ],
   imports: [
     BrowserModule,
